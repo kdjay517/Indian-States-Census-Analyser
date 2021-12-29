db351d1a8a99ccf956fb2f0dabad144e687e2011
kdjay517 <kdjay517@gmail.com>
2021-12-29 14:13:16 +0530

    Added : Analyser ro load indain states code information from csv

--- src/main/java/com/bridgelabz/indianstatesenses/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/indianstatesenses/CensusAnalyserException.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.indianstatesenses;
 
+
 public class CensusAnalyserException extends Exception {
     public enum ExceptionType {
         CENSUS_FILE_INCORRECT, CENSUS_INCORRECT_FILE_FORMAT, CENSUS_WRONG_DELIMITER, CENSUS_WRONG_HEADER;
