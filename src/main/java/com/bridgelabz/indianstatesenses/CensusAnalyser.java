57dfbdc131fb5fd7a9645d0b84728af46d812005
kdjay517 <kdjay517@gmail.com>
2021-12-29 13:08:31 +0530

    Added: Returning Custom Exception when wrong File given as input

--- src/main/java/com/bridgelabz/indianstatesenses/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/indianstatesenses/CensusAnalyser.java
@@ -11,21 +11,18 @@ import java.util.Iterator;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
-    public int loadIndiaCensusData(String csvFilePath) {
-        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
-            CsvToBean<IndiaCensusCSV>csvToBean = new CsvToBeanBuilder<IndiaCensusCSV>(reader)
-                    .withType(IndiaCensusCSV.class)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build();
-            Iterator<IndiaCensusCSV>iterator = csvToBean.iterator();
-            // iterator doesn't consume memory
-            Iterable<IndiaCensusCSV> csvIterable = () -> iterator;
-            int count = (int) StreamSupport.stream(csvIterable.spliterator(), true).count();
-            return count;
-        } catch (
-                IOException e) {
-            System.out.println(e);
-        }
-        return 0;
-    }
+	public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+			CsvToBean<IndiaCensusCSV> csvToBean = new CsvToBeanBuilder<IndiaCensusCSV>(reader)
+					.withType(IndiaCensusCSV.class).withIgnoreLeadingWhiteSpace(true).build();
+			Iterator<IndiaCensusCSV> iterator = csvToBean.iterator();
+			// iterator doesn't consume memory
+			Iterable<IndiaCensusCSV> csvIterable = () -> iterator;
+			int count = (int) StreamSupport.stream(csvIterable.spliterator(), true).count();
+			return count;
+		} catch (IOException e) {
+			throw new CensusAnalyserException(e.getMessage(),
+					CensusAnalyserException.ExceptionType.CENSUS_FILE_INCORRECT);
+		}
+	}
 }
